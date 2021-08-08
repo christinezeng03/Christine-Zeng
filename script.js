3e8e26cf73fab7a67792a6ec6eb273e001a8e61a
Glitch (portfolio0) <none>
2021-08-08 20:03:33 +0000

    🌾

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+This is your site JavaScript code - you can add interactivity and carry out processing
+- Initially the JS writes a message to the console, and moves a button you can add from the README
+*/
+
+// Print a message in the browser's dev tools console each time the page loads
+// Use your menus or right-click / control-click and choose "Inspect" > "Console"
+console.log("Hello! 🌎");
+
+
